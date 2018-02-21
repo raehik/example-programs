24744bcebca331ef44d35b7c09cc8362da8789f7
Ben Orchard <thefirstmuffinman@gmail.com>
2018-02-21 12:18:36 +0000

    update python examples to use new raehutils stuff

--- python-script-command-based.py
+++ python-script-command-based.py
@@ -3,23 +3,18 @@
 # Short description of the program/script's operation/function.
 #
 
-import sys, os, argparse, logging
 import raehutils
+import sys, os, argparse, logging
 
-class Program:
+class Program(raehutils.RaehBaseClass):
     def __init__(self):
         pass
-    def __deinit(self):
+
+    def _deinit(self):
         self.logger.debug("deinitialising...")
 
     ## CLI-related {{{
-    def __init_logging(self):
-        self.logger = logging.getLogger(os.path.basename(sys.argv[0]))
-        lh = logging.StreamHandler()
-        lh.setFormatter(logging.Formatter("%(name)s: %(levelname)s: %(message)s"))
-        self.logger.addHandler(lh)
-
-    def __parse_args(self):
+    def _parse_args(self):
         self.parser = argparse.ArgumentParser(description="Short description of the program/script's operation/function.")
         self.parser.add_argument("-v", "--verbose", help="be verbose", action="count", default=0)
         self.parser.add_argument("-q", "--quiet", help="be quiet (overrides -v)", action="count", default=0)
@@ -32,37 +27,15 @@ class Program:
                 description="Example command to show how to use subparsers.")
         subp_example.set_defaults(func=self.cmd_example_command)
 
-
         self.args = self.parser.parse_args()
-        if self.args.verbose == 1:
-            self.logger.setLevel(logging.INFO)
-        elif self.args.verbose >= 2:
-            self.logger.setLevel(logging.DEBUG)
-        if self.args.quiet >= 1:
-            # reset verbosity (to make verbose/quiet checks easier)
-            self.args.verbose = 0
-            self.logger.setLevel(logging.NOTSET)
 
-        # run the function of the matched subcommand
-        self.args.func()
-
-    def run(self):
-        """Run from CLI: parse arguments, execute command, deinitialise."""
-        self.__init_logging()
-        self.__parse_args()
-        self.__deinit()
+        self.args.verbose += 1 # force some verbosity
+        self._parse_verbosity()
     ## }}}
 
-    def fail(self, msg, ret):
-        """Exit with a message and a return code.
-
-        Should only be used for errors -- if you want to deinitialise and exit
-        safely, simply return from the main command.
-        """
-        self.logger.error(msg)
-        self.logger.debug("deinitialising...")
-        self.__deinit()
-        sys.exit(ret)
+    def main(self):
+        """Main entrypoint after program initialisation."""
+        self.args.func()
 
     def cmd_example_command(self):
         return True
--- python-script.py
+++ python-script.py
@@ -3,56 +3,28 @@
 # Short description of the program/script's operation/function.
 #
 
-import sys, os, argparse, logging
 import raehutils
+import sys, os, argparse, logging
 
-class Program:
+class Program(raehutils.RaehBaseClass):
     def __init__(self):
         pass
-    def __deinit(self):
+
+    def _deinit(self):
         self.logger.debug("deinitialising...")
 
     ## CLI-related {{{
-    def __init_logging(self):
-        self.logger = logging.getLogger(os.path.basename(sys.argv[0]))
-        lh = logging.StreamHandler()
-        lh.setFormatter(logging.Formatter("%(name)s: %(levelname)s: %(message)s"))
-        self.logger.addHandler(lh)
-
-    def __parse_args(self):
+    def _parse_args(self):
         self.parser = argparse.ArgumentParser(description="Short description of the program/script's operation/function.")
         self.parser.add_argument("-v", "--verbose", help="be verbose", action="count", default=0)
         self.parser.add_argument("-q", "--quiet", help="be quiet (overrides -v)", action="count", default=0)
         self.parser.add_argument("posarg", help="positional argument")
 
         self.args = self.parser.parse_args()
-        if self.args.verbose == 1:
-            self.logger.setLevel(logging.INFO)
-        elif self.args.verbose >= 2:
-            self.logger.setLevel(logging.DEBUG)
-        if self.args.quiet >= 1:
-            # reset verbosity (to make verbose/quiet checks easier)
-            self.args.verbose = 0
-            self.logger.setLevel(logging.NOTSET)
 
-    def run(self):
-        """Run from CLI: parse arguments, run main, deinitialise."""
-        self.__init_logging()
-        self.__parse_args()
-        self.main()
-        self.__deinit()
+        self.args.verbose += 1 # force some verbosity
+        self._parse_verbosity()
     ## }}}
-
-    def fail(self, msg, ret):
-        """Exit with a message and a return code.
-
-        Should only be used for errors -- if you want to deinitialise and exit
-        safely, simply return from main.
-        """
-        self.logger.error(msg)
-        self.logger.debug("deinitialising...")
-        self.__deinit()
-        sys.exit(ret)
 
     def main(self):
         """Main entrypoint after program initialisation."""
